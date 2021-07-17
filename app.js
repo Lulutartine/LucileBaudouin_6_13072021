b066b07066bdae034f4def130183a1f68e13aeb1
Lucile <lucile.baudouin@gmail.com>
2021-07-17 16:18:40 +0200

    app.js test fetch photographers profils on index

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+// Photographers profils
+fetch("./data.json")
+    .then(response => {
+        return response.json()
+    })
+
 // DOM ELEMENTS
 const mainContent = document.querySelector('.mainContent');
+
