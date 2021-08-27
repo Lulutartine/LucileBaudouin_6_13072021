4025129c16586e9a72a8ae3ebf585a4b4b45adc3
Lucile <lucile.baudouin@gmail.com>
2021-08-27 11:30:31 +0200

    photographers cards display function in index
    trial

--- app.js
+++ app.js
@@ -3,132 +3,145 @@ fetch("./data.json")
       return response.json();
    }) 
    .then(response2 => {
-	sessionStorage.setItem('data', JSON.stringify(response2));
-    init(response2);
-   });
+	   sessionStorage.setItem('data', JSON.stringify(response2));
+   init(response2);
+});
 
-// /**
-//  * @param domElement
-//  */
-class CreateDOMElement {
-	constructor(domElement, className, id, src) {
-    	this.domElement = domElement;
-       	this.className = className;
-
-    const elem = document.createElement(this.domElement);
-    elem.classList.add(this.className);
-	return elem;
-    }
- }
-
-class CreateHeader {
-    constructor() {
-    const header = new domElement('header');
-    const linkHome = new domElement('a');
-    const h1 = new domElement('h1');
-    const logo = new domElement('img');
-    const nav = new domElement('nav');
-    }
-}
-
-
-const image = new CreateDOMElement('img', 'logo');
-const div = new CreateDOMElement('div', 'bground');
-
-//DOM ELEMENTS
-const mainContent = document.querySelector('.mainContent');
-
-//___________//create
-//HEADER_____
-const header = document.createElement('header');
-const linkHome = document.createElement('a');
-const h1 = document.createElement('h1');
-const logo = document.createElement('img');
-const nav = document.createElement('nav');
-//___________//settings
-//HEADER_____
-header.classList.add('banner');
-linkHome.classList.add('logo');
-logo.scr = "FishEye_Photos/logo.png";
-logo.alt = "FishEye Home Page";
-nav.classList.add('topNav');
-nav.ariaLabel = "photographer categories";
-//___________//indent
-//HEADER_____
-header.append(linkHome);
-linkHome.append(logo);
-header.append(nav);
-nav.append(ul);
-
-const init = (data) => {
-   //boucle pour chaque photographes
-   data.photographers.forEach(photographer => {
-
-      //___________//create
-      //CARDS______
-      const card = document.createElement('a');
-      const image = document.createElement('img');
-      const contact = document.createElement('button');
-      const blockFlex = document.createElement('div');
-      const nameTitle = document.createElement('h2');
-      const para = document.createElement('p');
-      const place = document.createElement('span');
-      const gimmick = document.createElement('span');
-      const price = document.createElement('span');
-      const mainFlux = document.createElement('section');
-      //__________//create
-      //TAGS______
+// ________ DOM ELEMENTS  ________ //
+const createHeader = (data) => {
+   // ON FABRIQUE LES BALISES
+      const body = document.querySelector("body");
+      const header = document.querySelector("header");
+   
+      const hidden = document.createElement('div');
+      hidden.classList.add('hidden');
+      const hiddenLink = document.createElement('a');
+      hiddenLink.href = '#main';
+      hiddenLink.innerHTML = 'Passer au contenu';
+   
+      const logoLink = document.createElement('a');
+      const logo = document.createElement('img');
+      logoLink.href = './index.html';
+      logoLink.classList.add('logo');
+      logo.src = 'FishEye_Photos/logo.png';
+      logo.alt = 'FishEye Home Page';
+   
+      const h1 = document.createElement('h1');
+      h1.innerHTML = 'Nos photographes';
+   
+      const nav = document.createElement('nav');
       const ul = document.createElement('ul');
-   //**************************************************************************************/
-      //___________//settings
-      //CARDS______
-      card.classList.add('cards');
-      card.href = "photographer.html?id=" + photographer.id;
-      image.src = "FishEye_Photos/Photographers ID Photos/" + photographer.portrait;
-      image.alt = 'photo portrait de ' + photographer.name;
-      contact.classList.add('contact-btn');
-      contact.innerHTML = 'Contactez-moi'
-      blockFlex.classList.add('blockFlex');
-      nameTitle.innerHTML = photographer.name;
-      para.classList.add('descript');
-      place.classList.add('local');
-      place.innerHTML = photographer.city + ', ' + photographer.country;
-      gimmick.classList.add('depiction');
-      gimmick.innerHTML = photographer.tagline;
-      price.classList.add('price');
-      price.innerHTML = photographer.price + '€/jour';
-      mainFlux.classList.add('mainFlux');
-      //__________//settings
-      //TAGS______
+      nav.ariaLabel = 'photographer categories';
+      nav.classList.add('topNav');
       ul.classList.add('tagDesign');
-      /*********inDOM*********/
-      mainContent.append(card);
-   //**************************************************************************************/
-      //___________//indent
-      //CARDS______
+   
+      const fullTags = [];
+      data.photographers.forEach(photographer => {
+         photographer.tags.forEach(tag => {
+            fullTags.push(tag)
+         });
+      });
+   
+      const uniqueTags = fullTags.filter(function (item, pos) {
+         return fullTags.indexOf(item) == pos;
+      })
+   
+   // ON REMPLI lES BALISES
+      for (let i = 0; i < uniqueTags.length; i++) {
+         const tag = uniqueTags[i];
+         const li = document.createElement('li');
+         const btn = document.createElement('button');
+   
+         btn.classList.add('tagDesign__tag');
+         btn.innerHTML = `#${tag}`;
+         btn.id = tag;
+   
+         li.append(btn);
+         ul.append(li);
+      }
+   
+      hidden.append(hiddenLink);
+   
+      logoLink.append(logo);
+      nav.append(ul);
+   
+   // ON LES INSÈRE DANS LE DOM
+      header.append(logoLink);
+      header.append(nav);
+      header.append(h1);
+   
+      body.append(hidden);
+   }
+   
+const createCard = (photographerData) => {
+   // ON FABRIQUE LES BALISES
+      const card = document.createElement('a');
+      card.classList.add('cards');
+      card.href = 'photographer.html?id=' + photographerData.id;
+      card.id = photographerData.id;
+   
+      const image = document.createElement('img');
+      image.src = 'FishEye_Photos/Photographers ID Photos/' + photographerData.portrait;
+      image.alt = 'photo portrait de ' + photographerData.name;
+   
+      const cardDescription = document.createElement('div');
+      cardDescription.classList.add('cardDescription');
+   
+      const nameTitle = document.createElement('h2');
+      nameTitle.innerHTML = photographerData.name;
+   
+      const para = document.createElement('p');
+      para.classList.add('descript');
+   
+      const place = document.createElement('span');
+      place.classList.add('local');
+      place.innerHTML = photographerData.city + ', ' + photographerData.country;
+   
+      const depiction = document.createElement('span');
+      depiction.classList.add('depiction');
+      depiction.innerHTML = photographerData.tagline;
+   
+      const price = document.createElement('span');
+      price.classList.add('price');
+      price.innerHTML = photographerData.price + '€/jour';
+   
+      const ul = document.createElement('ul');
+      ul.classList.add('tagDesign');
+   
+   // ON REMPLI LES BALISES
       card.append(image);
-      card.append(contact);
-      card.append(blockFlex);
-      blockFlex.append(nameTitle);
-      blockFlex.append(para);
+   
       para.append(place);
-      para.append(gimmick);
+      para.append(depiction);
       para.append(price);
-      card.append(mainFlux);
-
-      //__________//indent
-      //TAGS______
-      for (let i = 0; i < photographer.tags.length; i++) {
-         const li = document.createElement('li')
+   
+      cardDescription.append(nameTitle);
+      cardDescription.append(para);
+   
+      for (let i = 0; i < photographerData.tags.length; i++) {
+         const li = document.createElement('li');
          const tag = document.createElement('span');
-
-         tag.classList.add('tagDesign__tag');
-         tag.innerHTML = '#' + photographer.tags[i];
-
+   
+         tag.classList.add('tagDesign__tag2');
+         tag.innerHTML = '#' + photographerData.tags[i];
+   
          li.append(tag);
          ul.append(li);
       }
-      blockFlex.append(ul);
-   })
-}
+      cardDescription.append(ul);
+   
+      card.append(cardDescription);
+   
+   // ON INSÈRE TOUT DANS LE DOM
+      const main = document.querySelector('main');
+      main.append(card);
+   }
 
+const initIndex = (data) => {
+      //boucle pour chaque photographes
+      data.photographers.forEach(photographer => {
+         createCard(photographer);
+      });
+      
+      createHeader(data)
+   };
