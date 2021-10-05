8a21af9ee84148038c4c7bf4354456b0ca645c48
Lucile <lucile.baudouin@gmail.com>
2021-10-05 11:10:57 +0200

    Webpack installation

--- assets/js/trial.js
+++ assets/js/trial.js
@@ -6,13 +6,13 @@ function append(parent, el) {
   return parent.appendChild(el);
 }
 
-const cards = document.getElementById('photographersData');
+const photographerData = document.getElementById('photographersData');
 const url = "./data.json";
 
 fetch(url)
 .then((response) => response.json())
 .then(function(data) {
-  let photographersData = data.results;
+  let photographerData = data.results;
   return photographersData.map(function(photographer) {
     let div = createNode('photographercard');
     let img = createNode('img');
@@ -21,7 +21,7 @@ fetch(url)
     span.innerHTML = `${photographer.name} ${photographer.id}`;
     append(div, img);
     append(div, span);
-    append(cards, div);
+    append(photographercard, div);
   })
 })
 .catch(function(error) {
